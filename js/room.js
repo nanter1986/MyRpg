dea82cc5ee20616f2f5f364cfffd1fad8c95246d
nanter1986 <nanter1986@gmail.com>
2018-08-31 08:09:48 +0300

    found storage methodc

--- js/room.js
+++ js/room.js
@@ -181,6 +181,15 @@ function Room(game){
 		console.log("tap/"+pointer.x+"/"+pointer.y);
 	};
 	Room.saveData=function(){
+		var testObject = { 'one': 1, 'two': 2, 'three': 3 };
+
+		// Put the object into storage
+		localStorage.setItem('testObject', JSON.stringify(testObject));
+
+		// Retrieve the object from storage
+		var retrievedObject = localStorage.getItem('testObject');
+
+		console.log('retrievedObject: ', JSON.parse(retrievedObject));
 
 	};
 	Room.enterDoor=function(){
